5724d54f939f5cc74c1ba26f385f468dfe5537c4
s-gobo <144197685+s-gobo@users.noreply.github.com>
2024-09-30 18:22:30 -0700

    Update index.js

--- noise/index.js
+++ noise/index.js
@@ -16,16 +16,11 @@ let noise = {
   }(),
   
   dp: function(v1, v2) {
-    if (v1.x * v2.x + v1.y * v2.y > 1) {
-      console.log(v1, v2);
-    }
-    return v1.x * v2.x + v1.y * v2.y;
+    return v1.x * v2.x + v1.y + v1.y;
   },
   
   in: function(a, b, wt) {
-    wt = (wt + 1) / 2;
     wt = 6 * wt ** 5 - 15 * wt ** 4 + 10 * wt ** 3;
-    wt = wt * 2 - 1;
     return a + (b - a) * wt;
   },
   
@@ -38,11 +33,12 @@ let noise = {
     let pur = {x: r - x, y: d - y};
     let pdl = {x: l - x, y: u - y};
     let pdr = {x: r - x, y: d - y};
-    let dpul = this.dp(pul, this.memory[[u, l]]);
-    let dpur = this.dp(pur, this.memory[[u, r]]);
-    let dpdl = this.dp(pdl, this.memory[[d, l]]);
-    let dpdr = this.dp(pdr, this.memory[[d, r]]);
-    //console.log(dpul, dpur, dpdl, dpdr);
+    // console.log(this.memory[[l, u]], this.memory[[r, u]]);
+    let dpul = this.dp(pul, this.memory[[l, u]]);
+    let dpur = this.dp(pur, this.memory[[r, u]]);
+    let dpdl = this.dp(pdl, this.memory[[l, d]]);
+    let dpdr = this.dp(pdr, this.memory[[r, d]]);
+    // console.log(dpul, dpur);
     let reu = this.in(dpul, dpur, x - l);
     let red = this.in(dpdl, dpdr, x - l);
     let re = this.in(reu, red, y - d);
@@ -50,21 +46,29 @@ let noise = {
   },
 };
 
-let renderKey = function(number) {
-  return number >= 1? "M":
-    number >= 0.6? "X":
-    number >= 0.2? "+":
-    number >= -0.2? "-":
-    number >= -0.6? "-":
-    number >= -1? ".":
-    " ";
-}
-
-for (let i = 0; i < 10; i += 0.1) {
-  let string = "";
-  for (let j = 0; j < 10; j += 0.1) {
-    string += renderKey(noise.get(i, j));
+for (let i = 0; i < 10; i += 0.05) {
+  let count = 40 + Math.floor(noise.get(i, 0) * 20)
+  try {
+    console.log("X".repeat(count));
+  } catch {
+    console.log(count);
   }
-  //console.log(string);
 }
 
+// let renderKey = function(number) {
+//   return number >= 1? "@":
+//     number >= 0.6? "M":
+//     number >= 0.2? "X":
+//     number >= -0.2? "+":
+//     number >= -0.6? "-":
+//     number >= -1? ".":
+//     " ";
+// }
+
+// for (let i = 0; i < 10; i += 0.1) {
+//   let string = "";
+//   for (let j = 0; j < 10; j += 0.1) {
+//     string += renderKey(noise.get(i, j));
+//   }
+//   console.log(string);
+// }
