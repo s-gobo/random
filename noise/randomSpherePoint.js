af52971d0e8708a68d6115198a34e254fa96b7ab
s-gobo <144197685+s-gobo@users.noreply.github.com>
2024-10-01 16:17:28 -0700

    Create randomSpherePoint.js

---
+++ noise/randomSpherePoint.js
@@ -0,0 +1,22 @@
+let randomSpherePoint = function() {
+  let x, y, z, dist;
+  //get a random point in a unit sphere
+  do {
+    x = 2 * Math.random() - 1;
+    y = 2 * Math.random() - 1;
+    z = 2 * Math.random() - 1;
+    dist = Math.sqrt(x ** 2 + y ** 2 + z ** 2);
+  } while (dist > 1);
+  //move the point away from the center
+  dist = 1 / dist;
+  x *= dist;
+  y *= dist;
+  z *= dist;
+  
+  return {x: x, y: y, z: z};
+}
+
+point = randomSpherePoint();
+console.log(point);
+//verify is on the surface
+console.log(Math.sqrt(point.x ** 2 + point.y ** 2 + point.z ** 2));
