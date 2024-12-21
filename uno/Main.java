ab4beef4ea2296caaea1b4265eee62f3b4c7b346
s-gobo <144197685+s-gobo@users.noreply.github.com>
2024-12-20 23:10:23 -0800

    Bots have names now

--- uno/Main.java
+++ uno/Main.java
@@ -397,6 +397,8 @@ class Uno {
 }
 
 class Player {
+  private static final boolean COMPUTER_ALIAS = true;
+  
   private boolean isBot;
   private String name;
   private Uno game;
@@ -404,12 +406,16 @@ class Player {
   private Card[] hand;
   private int handSize;
   
+  private static final String[] BOT_NAMES = {"James", "Mary", "Michael", "Patricia", "Robert", "Jennifer", "John", "Linda", "David", "Elizabeth", "William", "Barbara", "Richard", "Susan", "Joseph", "Jessica", "Thomas", "Karen", "Christopher", "Sarah", "Charles", "Lisa", "Daniel", "Nancy", "Matthew", "Sandra", "Anthony", "Betty", "Mark", "Ashley", "Donald", "Emily", "Steven", "Kimberly", "Andrew", "Margaret", "Paul", "Donna", "Joshua", "Michelle", "Kenneth", "Carol", "Kevin", "Amanda", "Brian", "Melissa", "Timothy", "Deborah", "Ronald", "Stephanie", "George", "Rebecca", "Jason", "Sharon", "Edward", "Laura", "Jeffrey", "Cynthia", "Ryan", "Dorothy", "Jacob", "Amy", "Nicholas", "Kathleen", "Gary", "Angela", "Eric", "Shirley", "Jonathan", "Emma", "Stephen", "Brenda", "Larry", "Pamela", "Justin", "Nicole", "Scott", "Anna", "Brandon", "Samantha", "Benjamin", "Katherine", "Samuel", "Christine", "Gregory", "Debra", "Alexander", "Rachel", "Patrick", "Carolyn", "Frank", "Janet", "Raymond", "Maria", "Jack", "Olivia", "Dennis", "Heather", "Jerry", "Helen", "Tyler", "Catherine", "Aaron", "Diane", "Jose", "Julie", "Adam", "Victoria", "Nathan", "Joyce", "Henry", "Lauren", "Zachary", "Kelly", "Douglas", "Christina", "Peter", "Ruth", "Kyle", "Joan", "Noah", "Virginia", "Ethan", "Judith", "Jeremy", "Evelyn", "Christian", "Hannah", "Walter", "Andrea", "Keith", "Megan", "Austin", "Cheryl", "Roger", "Jacqueline", "Terry", "Madison", "Sean", "Teresa", "Gerald", "Abigail", "Carl", "Sophia", "Dylan", "Martha", "Harold", "Sara", "Jordan", "Gloria", "Jesse", "Janice", "Bryan", "Kathryn", "Lawrence", "Ann", "Arthur", "Isabella", "Gabriel", "Judy", "Bruce", "Charlotte", "Logan", "Julia", "Billy", "Grace", "Joe", "Amber", "Alan", "Alice", "Juan", "Jean", "Elijah", "Denise", "Willie", "Frances", "Albert", "Danielle", "Wayne", "Marilyn", "Randy", "Natalie", "Mason", "Beverly", "Vincent", "Diana", "Liam", "Brittany", "Roy", "Theresa", "Bobby", "Kayla", "Caleb", "Alexis", "Bradley", "Doris", "Russell", "Lori", "Lucas", "Tiffany"};
+  private static int[] usedBotNames = new int[BOT_NAMES.length];
+  private static int usedBotNamesSize = 0;
+  
   public Player(Uno game, boolean bot) {
     isBot = bot;
     
     // name bots and players
     if (isBot) {
-      name = "Bot" + (int) (1000 * Math.random());
+      name = nameBot();
     } else {
       name = "You";
     }
@@ -837,6 +843,31 @@ class Player {
           Main.logTurn(this, 1, null);
         }
       }
+    }
+  }
+  
+  private static String nameBot() {
+    if (!COMPUTER_ALIAS) {
+      return "Bot" + (int) (1000 * Math.random());
+    }
+    // if all 200 names are somehow used up, repeat names.
+    if (BOT_NAMES.length == usedBotNamesSize) {
+      usedBotNamesSize = 0;
+    }
+    genName: while (true) {
+      int choice = (int) (BOT_NAMES.length * Math.random());
+      
+      // if the name's taken, try again
+      for (int i = 0; i < usedBotNamesSize; i++) {
+        if (usedBotNames[i] == choice) {
+          continue genName;
+        }
+      }
+      
+      // the name is found
+      usedBotNames[usedBotNamesSize] = choice;
+      usedBotNamesSize++;
+      return BOT_NAMES[choice];
     }
   }
 }
